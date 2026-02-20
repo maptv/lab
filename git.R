d65e4a86d6b25318a753deec33c5bd6f8274cd2a
maptv <mlaptev@ccbcmd.edu>
2026-02-20 02:52:24 +0000

    add git.R back after pull with rebase

---
+++ git.R
@@ -0,0 +1,31 @@
+install.packages("usethis")
+install.packages("gitcreds")
+
+usethis::use_git_config(
+  user.name = "maptv",
+  user.email = "mlaptev@ccbcmd.edu",
+  core.editor = "nano",
+)
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+
+usethis::git_default_branch_rename(from = NULL, to = "main")
+
+usethis::use_git_remote(
+  name = "origin",
+  url = "https://github.com/maptv/lab",
+)
+usethis::git_remotes()
+usethis::git_default_branch_configure("main")
+
+usethis::git_default_branch()
+usethis::git_default_branch_rename("master")
+
+usethis::use_git_remote(url = "https://github.com/maptv/lab", overwrite = TRUE)
+
+usethis::use_github(
+  "maptv/lab"
+)
+
